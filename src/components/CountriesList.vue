6e45714508b0bfca816cc37ccb67c6aa06bf1257
Eoligan <106335980+Eoligan@users.noreply.github.com>
2023-08-03 12:31:41 +0000

    CountriesList component almost finished

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="bg-blue-500">
+  <div>
     <section>
-      <ul>
-        <li v-for="country in sortedCountriesList" :key="country.id">
+      <ul class="">
+        <li
+          class="border border-gray-400 p-10 grid place-items-center hover:bg-gray-200 cursor-pointer uppercase font-bold text-sm"
+          v-for="country in sortedCountriesList"
+          :key="country.id"
+        >
           {{ country }}
+          <img
+            class="pt-4"
+            src="https://flagpedia.net/data/flags/icon/72x54/fr.png"
+            alt=""
+          />
         </li>
       </ul>
     </section>
@@ -21,17 +30,14 @@ const fetchCountries = async (total) => {
   );
   const data = await response.json();
   for (let i = 0; i < data.length; i++) {
-    // countriesList[i].value = data.common;
     countriesList.value.push(data[i].name.common);
-
-    // console.log(countriesList.value);
   }
 };
 
 onMounted(() => {
   fetchCountries();
 });
-console.log(countriesList.value.sort());
+
 const sortedCountriesList = computed(() => {
   return countriesList.value.sort();
 });
